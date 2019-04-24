03896b38c2526337b0f7138f04464e1a46210887
Maria <maria.vexlard-ext@sagemcom.com>
2019-04-24 11:56:37 +0200

    refactoring

--- spell.py
+++ spell.py
@@ -5,96 +5,94 @@ import re
 import json
 import pickle
 
-class lcsobj():
 
-    def __init__(self, objid, seq, lineid, refmt):
+class LCSObj:
+    def __init__(self, obj_id, seq, line_id, refmt):
         self._refmt = refmt
-        if isinstance(seq, str) == True:
-            self._lcsseq = re.split(self._refmt, seq.lstrip().rstrip())
+        if isinstance(seq, str):
+            self._lcs_seq = re.split(self._refmt, seq.strip())
         else:
-            self._lcsseq = seq
-        self._lineids = [lineid]
-        self._pos = []
+            self._lcs_seq = seq
+        self._line_ids = [line_id]
+        self._pos = []  # placeholder positions
         self._sep = "	"
-        self._id = objid
+        self._id = obj_id
         return
 
-    def getlcs(self, seq):
-        if isinstance(seq, str) == True:
-            seq = re.split(self._refmt, seq.lstrip().rstrip())
+    def __len__(self):
+        return len(self._lcs_seq)
+
+    def get_lcs(self, seq):
+        if isinstance(seq, str):
+            seq = re.split(self._refmt, seq.strip())
         count = 0
-        lastmatch = -1
-        for i in range(len(self._lcsseq)):
-            #if self._lcsseq[i] == '*':
-            if self._ispos(i) == True:
+        last_match = -1
+        for i in range(len(self._lcs_seq)):
+            if i in self._pos:
                 continue
-            for j in range(lastmatch+1, len(seq)):
-                if self._lcsseq[i] == seq[j]:
-                    lastmatch = j
+            for j in range(last_match+1, len(seq)):
+                if self._lcs_seq[i] == seq[j]:
+                    last_match = j
                     count += 1
                     break
         return count
 
-    def insert(self, seq, lineid):
-        if isinstance(seq, str) == True:
-            seq = re.split(self._refmt, seq.lstrip().rstrip())
-        self._lineids.append(lineid)
-        temp = ""
-        lastmatch = -1
+    def insert(self, seq, line_id):
+        if isinstance(seq, str):
+            seq = re.split(self._refmt, seq.strip())
+        self._line_ids.append(line_id)
+        temp = []
+        last_match = -1
         placeholder = False
 
-        for i in range(len(self._lcsseq)):
-            #if self._lcsseq[i] == '*':
-            if self._ispos(i) == True:
+        for i, token in enumerate(self._lcs_seq):
+
+            # adjacent placeholders are not allowed
+            if i in self._pos:
                 if not placeholder:
-                    temp = temp + "* "
+                    temp.append("*")
                 placeholder = True
                 continue
-            for j in range(lastmatch+1, len(seq)):
-                if self._lcsseq[i] == seq[j]:
+
+            # else :
+            for j in range(last_match+1, len(seq)):
+                if seq[j] == token:
                     placeholder = False
-                    temp = temp + self._lcsseq[i] + " "
-                    lastmatch = j
+                    temp.append(token)
+                    last_match = j
                     break
                 elif not placeholder:
-                    temp = temp + "* "
+                    temp.append("*")
                     placeholder = True
-        temp = temp.lstrip().rstrip()
-        self._lcsseq = re.split(" ", temp)
-
+                    
+        self._lcs_seq = temp
         self._pos = self._get_pos()
         self._sep = self._get_sep()
 
-    def tojson(self):
-        temp = ""
-        for i in self._lcsseq:
-            temp = temp + i + " "
-        ret = {}
-        ret["lcsseq"] = temp
-        ret["lineids"] = self._lineids
-        ret["postion"] = self._pos
-        return json.dumps(ret)
-
-    def length(self):
-        return len(self._lcsseq)
+    def to_json(self):
+        return json.dumps({
+            "lcsseq": self._lcs_seq,
+            "lineids": self._line_ids,
+            "position": self._pos
+        })
 
     def param(self, seq):
-        if isinstance(seq, str) == True:
-            seq = re.split(self._refmt, seq.lstrip().rstrip())
+        if isinstance(seq, str):
+            seq = re.split(self._refmt, seq.strip())
 
         j = 0
         ret = []
-        for i in range(len(self._lcsseq)):
+        for i in range(len(self)):
             slot = []
-            if self._ispos(i) == True:
+            if i in self._pos:
                 while j < len(seq):
-                    if i != len(self._lcsseq)-1 and self._lcsseq[i+1] == seq[j]:
+                    if i != len(self._lcs_seq)-1 and self._lcs_seq[i + 1] == seq[j]:
                         break
                     else:
                         slot.append(seq[j])
-                    j+=1
+                    j += 1
                 ret.append(slot)
-            elif self._lcsseq[i] != seq[j]:
+            elif self._lcs_seq[i] != seq[j]:
                 return None
             else:
                 j += 1
@@ -105,9 +103,9 @@ class lcsobj():
             return ret
 
     def re_param(self, seq):
-        if isinstance(seq, list) == True:
+        if isinstance(seq, list):
             seq = ' '.join(seq)
-        seq = seq.lstrip().rstrip()
+        seq = seq.strip()
 
         ret = []
         print(self._sep)
@@ -115,40 +113,26 @@ class lcsobj():
         p = re.split(self._sep, seq)
         for i in p:
             if len(i) != 0:
-                ret.append(re.split(self._refmt, i.lstrip().rstrip()))
+                ret.append(re.split(self._refmt, i.strip()))
         if len(ret) == len(self._pos):
             return ret
         else:
             return None
 
-
-
-    def _ispos(self, idx):
-        for i in self._pos:
-            if i == idx:
-                return True
-        return False
-
-    def _tcat(self, seq, s, e):
-        sub = ''
-        for i in range(s, e + 1):
-            sub += seq[i] + " "
-        return sub.rstrip()
-
     def _get_sep(self):
         sep_token = []
         s = 0
         e = 0
-        for i in range(len(self._lcsseq)):
-            if self._ispos(i) == True:
+        for i in range(len(self)):
+            if i in self._pos:
                 if s != e:
-                    sep_token.append(self._tcat(self._lcsseq, s, e))
+                    sep_token.append(" ".join(self._lcs_seq[s:e+1]))
                 s = i + 1
                 e = i + 1
             else:
                 e = i
-            if e == len(self._lcsseq) - 1:
-                sep_token.append(self._tcat(self._lcsseq, s, e))
+            if e == len(self) - 1:
+                sep_token.append(" ".join(self._lcs_seq[s:e+1]))
                 break
 
         ret = ""
@@ -160,79 +144,80 @@ class lcsobj():
         return ret
 
     def _get_pos(self):
-        pos = []
-        for i in range(len(self._lcsseq)):
-            if self._lcsseq[i] == '*':
-                pos.append(i)
-        return pos
+        return [i for i, x in enumerate(self._lcs_seq) if x == '*']
 
     def get_id(self):
         return self._id
 
-class lcsmap():
 
+class LCSMap:
     def __init__(self, refmt):
         self._refmt = refmt
         self._lcsobjs = []
         self._lineid = 0
         self._id = 0
-        return
+
+    def __len__(self):
+        return len(self._lcsobjs)
 
     def insert(self, entry):
-        seq = re.split(self._refmt, entry.lstrip().rstrip())
+        seq = re.split(self._refmt, entry.strip())
         obj = self.match(seq)
-        if obj == None:
-            self._lineid += 1
-            obj = lcsobj(self._id, seq, self._lineid, self._refmt)
+
+        if obj is None:
+            obj = LCSObj(self._id, seq, self._lineid, self._refmt)
             self._lcsobjs.append(obj)
             self._id += 1
         else:
-            self._lineid += 1
             obj.insert(seq, self._lineid)
-
+        self._lineid += 1
         return obj
 
     def match(self, seq):
-        if isinstance(seq, str) == True:
-            seq = re.split(self._refmt, seq.lstrip().rstrip())
-        bestmatch = None
-        bestmatch_len = 0
-        seqlen = len(seq)
+        """
+        Match a sequence with an object in the map
+        :param seq: list or str
+        :return: LCSObj or None
+        """
+        if isinstance(seq, str):
+            seq = re.split(self._refmt, seq.strip())
+        best_match = None
+        best_match_len = 0
+        seq_len = len(seq)
         for obj in self._lcsobjs:
-            objlen = obj.length()
-            if objlen < seqlen/2 or objlen > seqlen*2: continue
+            obj_len = len(obj)
+            if obj_len < seq_len/2 or obj_len > seq_len*2:
+                continue
 
-            l = obj.getlcs(seq)
-            if l >= seqlen/2 and l > bestmatch_len:
-                bestmatch = obj
-                bestmatch_len = l
-        return bestmatch
+            lcs = obj.get_lcs(seq)
+            if lcs >= seq_len/2 and lcs > best_match_len:
+                best_match = obj
+                best_match_len = lcs
+        return best_match
 
-    def objat(self, idx):
+    def obj_at(self, idx):
         return self._lcsobjs[idx]
-
-    def size(self):
-        return len(self._lcsobjs)
 
     def dump(self):
         for i in self._lcsobjs:
             print(i.tojson())
 
 
-def save(filename, spell_lcsmap):
-    if type(spell_lcsmap) == lcsmap:
-        with open(filename,'wb') as f:
-            pickle.dump(spell_lcsmap, f)
+def save(filename, spell_lcs_map):
+    if type(spell_lcs_map) == LCSMap:
+        with open(filename, 'wb') as f:
+            pickle.dump(spell_lcs_map, f)
     else:
-        if __debug__ == True:
-            print("%s isnt slm object"%filename)
+        if __debug__:
+            print("%s isnt slm object" % filename)
+
 
 def load(filename):
-    with open(filename,'rb') as f:
+    with open(filename, 'rb') as f:
         slm = pickle.load(f)
-        if type(slm) == lcsmap:
+        if type(slm) == LCSMap:
             return slm
         else:
-            if __debug__ == True:
-                print("%s isnt slm object"%filename)
+            if __debug__:
+                print("%s isnt slm object" % filename)
             return None
